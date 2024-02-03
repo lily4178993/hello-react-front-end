83eca87b0b53f98c1c426dc0bbf1626c69097140
lily4178993 <nellytelli@gmail.com>
2024-02-03 23:48:53 +0000

    Add Greeting component and refresh button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { useDispatch } from 'react-redux';
 import './App.css';
+import Greeting from './components/Greeting';
+import { fetchGreeting } from './redux/slices/greeting/greetingSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const refreshGreeting = () => {
+    dispatch(fetchGreeting());
+  };
+
   return (
-    <>
-      <h1>Hello Vite!</h1>
-    </>
+    <div className="text-center text-red-800">
+      <div className="h-36">
+        <Greeting />
+      </div>
+      <button type="button" onClick={refreshGreeting} className="mt-8">Refresh</button>
+    </div>
   );
 }
 
